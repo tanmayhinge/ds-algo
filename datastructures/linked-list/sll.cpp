21c8b8996a9509724692df8accf7497e9cc4e852
tanmayhinge <hingetanmay@gmail.com>
2021-02-28 22:05:24 +0530

    start1C,hashcode

--- datastructures/linked-list/sll.cpp
+++ datastructures/linked-list/sll.cpp
@@ -1,123 +1,39 @@
-#include <iostream>
+#include<bits/stdc++.h>
+#define ll long long
 using namespace std;
 
-struct Node {
-	int data;
-	Node* next;
-};
-
-Node* head = new Node();
-
-
-void printList(){
-	Node* temp = head;
-	while(temp != NULL){
-		cout << temp -> data << " ";
-		temp = temp-> next;
-	}
-	cout << endl;
-}
-
-void Reverse(){
-	Node* current = head;
-	Node* prev = NULL;
-	Node* neXt;
-	while(current!=NULL){
-		neXt=current->next;
-		current->next=prev;
-		prev=current;
-		current=neXt;
-	}
-	
-	head = prev;
-}
-
-void insertBeginning(int num){
-	Node * temp = new Node();
-	temp -> data = num;
-	temp -> next = head;
-	head = temp;
-}
-
-void Insert(int num, int position){
-
-	Node* temp1 = new Node();
-	temp1->data= num;
-	temp1->next=NULL;
-	if(position==1){
-		temp1->next=head;
-		head=temp1;
-		return;
-	}
-	Node* temp2 = head;
-	for(int i=0; i < position-2; ++i){
-		temp2 = temp2 -> next;
-	}
-	temp1 -> next = temp2 -> next;
-	temp2 -> next = temp1;
-
-}
-
-void Delete(int pos){
-	Node* temp1 = head;
-	if(pos == 1){
-		head = temp1 -> next;
-		delete(temp1);
-		return;
-	}
-	for (int i = 0; i < pos-2; ++i)	
-		temp1 = temp1->next;
-	Node* temp2 = temp1->next;
-	temp1->next = temp2->next;
-	delete temp2;
-}
-
 int main(){
-	head = NULL;
-	//cout << "How many interations? : " << endl;
-	int n, x, p;
-	while(true){
-		cout << "Choose:" << endl;
-		cout << "----------------" << endl;
-		cout << "1: Insert Beginning" << endl;
-		cout << "2: Insert at Nth position" << endl;
-		cout << "3. Delete from nth posn" <<endl;
-		cout << "4. Reverse" << endl;
-		cout << "5: Exit" << endl;
-		cout << "----------------" << endl;
-		int s;
-		cin >> s;
-		switch (s)
-		{
-		case 1:
-			cout << "Which number:" << endl;
-			cin >> x;
-			insertBeginning(x);
-			printList();
-			break;
-		case 2:
-			cout << "Which number:" << endl;
-			cin >> x;
-			cout << "Which position:" << endl;
-			cin >> p;
-			Insert(x,p);
-			printList();
-			break;
-		case 3:
-			cout << "Which position?" << endl;
-			cin >> p;
-			Delete(p);
-			printList();
-			break;
-		case 4:
-			Reverse();
-			printList();
-			break;
-		case 5:
-			exit(0);
-			break;
-		default:
-			break;
-		}
-	}
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t, ans=0;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        int a[n];
+        for(int i=0; i<n; ++i){
+            cin>>a[i];
+
+    int first, second; 
+    if (a[0] > a[1]) 
+    { 
+        first = a[0]; 
+        second = a[1]; 
+    } 
+    else
+    { 
+        first = a[1]; 
+        second = a[0]; 
+    }
+    ans = first*second+first-second;
+
+            for(int j=i; j<n; ++j){
+                if(a[i]!=a[j]){
+                    ans=max(a[i]*a[j]+a[i]-a[j], ans);
+                }
+            }
+        }
+        cout<<ans<<endl;
+    }
+    return 0;
 }
